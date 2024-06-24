2e0c99bf9b12129e6afd5eecb22aeff3e22c5869
anacarolmag <150815052+anacarolmag@users.noreply.github.com>
2024-06-24 13:42:13 -0300

    Add files via upload

--- popups.py
+++ popups.py
@@ -1,35 +1,18 @@
 from kivy.uix.popup import Popup
-from kivy.uix.label import Label
 
 class ModbusPopup(Popup):
     """
     Popup da configuração do protocolo MODBUS
     """
-    _info_lb = None
-    def __init__(self, server_ip, server_port, **kwargs):
-
-        """
-        Construtor da classe ModbusPopup
-        """
-        super().__init__(**kwargs) # Inicializar o construtor da classe base para dar certo(isso para o kivy)
-        self.ids.txt_ip.text = str(server_ip)
-        self.ids.txt_port.text = str(server_port)
-
-    def setInfo(self, message): # Para criar widget dinâmico, durante o tempo de execução
-        self._info_lb = Label(text=message)
-        self.ids.layout.add_widget(self._info_lb)
-
-    def clearInfo(self):
-        if self._info_lb is not None:
-            self.ids.layout.remove_widget(self._info_lb)
+    pass
 
 class ScanPopup(Popup):
     """
-    Popup da configurção do tempo de varredura
+    Popup da configuração do tempo de varredura
     """
-    def __init__(self, scantime, **kwargs): # scantime: tempo de varredura / **kwargs: receber múltiplos argumetos sem ter que listar eles
+    def __init__(self, scantime,**kwardgs): #**kwardgs: maneira de receber multiplos argumentos sem ter que declara-los aqui
         """
-        Construtor da classe ScanPopup
+        Constri=utor da classe ScanPopup
         """
-        super().__init__(**kwargs) # Inicializar o construtor da classe base para dar certo(isso para o kivy)
+        super().__init__(**kwardgs)
         self.ids.txt_st.text = str(scantime)
