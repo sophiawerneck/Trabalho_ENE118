d8a8902f6521c21667ccfaa136e39da0a70539ae
Sophia Werneck <78826752+sophiawerneck@users.noreply.github.com>
2024-07-08 16:44:00 -0300

    Update popups.py

--- popups.py
+++ popups.py
@@ -1,6 +1,7 @@
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
+from kivy_garden.graph import LinePlot
 
 class ModbusPopup(Popup):
     """
